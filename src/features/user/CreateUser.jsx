1b2342aa0650f9af574a26ba676415614ffbff5a
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-01-29 13:29:05 +0200

    Added react-redux, reduc-toolkit and created userslice

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 const inputStyle =
   "md:6px w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none  focus:ring focus:ring-yellow-400 md:px-3";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return null;
+    dispatch(updateName(username));
+    setUsername("");
+    navigate("/menu");
   }
 
   return (
