cd23a8c5f3ef41e44bbcd02a4e254c20856f0aa3
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-02-01 11:43:43 +0200

    Added the functionality to get the user address by Geolocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -7,6 +7,8 @@ import { clearCart, getCartItems, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../Store";
 import { formatCurrency } from "../../utils/helpers";
+import { getAddress } from "../../services/apiGeocoding";
+import { fetchAddress } from "../user/userSlice";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -30,9 +32,9 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
 
   const dispatch = useDispatch();
-  if (isSubmitting) {
-    dispatch(clearCart);
-  }
+  // if (isSubmitting) {
+  //   dispatch(clearCart);
+  // }
   const formErrors = useActionData();
 
   // Cart data to be submitted
@@ -49,7 +51,15 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
+      <Button
+        type="small"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(fetchAddress());
+        }}
+      >
+        Get position
+      </Button>
       <Form method="POST">
         <div className="mb-5 flex flex-col  gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
